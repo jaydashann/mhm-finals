f914022f2b1b1a11b63658fb7d92511c8756038f
Jay-ann Alorro <85263396+jaydashann@users.noreply.github.com>
2025-05-19 13:31:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ import joblib
 # models
 url_1 = "https://drive.google.com/uc?id=1RjO6e_fI8NUT6F3zPs12qHYLOxqXIVOe"
 model_1 = "autoencoder.h5"
-gdown.download(url_1, model_1, quiet=False)
+#gdown.download(url_1, model_1, quiet=False)
 
 url_2 = "https://drive.google.com/uc?id=17jofX6sh8ennWJ_mboLyZxmcvJ2eniod"
 model_2 = "custom_dcnn_model.h5"
-gdown.download(url_2, model_2, quiet=False)
+#gdown.download(url_2, model_2, quiet=False)
 
 # scaler
 url_scaler = "https://drive.google.com/uc?id=1VS_8Se0KRqanfxYxTgLFi4fcv_lCEfjQ"
 scaler_file = "scaler.pkl"
-gdown.download(url_scaler, scaler_file, quiet=False)
+#gdown.download(url_scaler, scaler_file, quiet=False)
 scaler = joblib.load(scaler_file)
 
 # csv preprocessing
